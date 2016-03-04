125a3dd82d03bbce9dfbf3c8905bbaf4e463844c
Séra Bálint <sera.balint@e-vista.hu>
2016-03-04 10:26:48 +0100

    xml link collector setup

---
+++ src/XmlLinkCollector.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: balint
+ * Date: 2016. 03. 04.
+ * Time: 9:02
+ */
+
+namespace Evista\CrawlCacheWarmup;
+
+
+class XmlLinkCollector
+{
+    private $xml = '';
+    /**
+     * @param string $xml
+     */
+    public function construct($xml)
+    {
+        $this->xml = $xml;
+    }
+
+    public function collectURLs()
+    {
+
+    }
+}
